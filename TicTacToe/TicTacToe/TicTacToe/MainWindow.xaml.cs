86b09ddf4cbf371494e86298154b9e6b7adae93e
N76VZ\MichelleF <kogelmichelle@gmail.com>
2018-06-10 14:08:18 -0400

    Added buttons to the GUI. Tried to change the content of the button when clicked. ISSUE: The images are resources to the window and are being treated as single instances so the buttons get updated once. Which is incorrect. Changed the size of the images being used so that they are squares.

--- TicTacToe/TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -20,11 +20,66 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Board theBoard;
         public MainWindow()
         {
             InitializeComponent();
 
-            var theBoard = new Board();
+            theBoard = new Board();
+        }
+
+        private void btn_topleft_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(0, 0);
+            btn_topleft.Content = FindResource(theBoard.Grid[0, 0].ToString());
+        }
+
+        private void btn_topcentre_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(0, 1);
+            btn_topcentre.Content = FindResource(theBoard.Grid[0, 1].ToString());
+        }
+
+        private void btn_topright_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(0, 2);
+            btn_topright.Content = FindResource(theBoard.Grid[0, 2].ToString());
+        }
+
+        private void btn_left_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(1, 0);
+            btn_left.Content = FindResource(theBoard.Grid[1, 0].ToString());
+        }
+
+        private void btn_centre_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(1, 1);
+            btn_centre.Content = FindResource(theBoard.Grid[1, 1].ToString());
+        }
+
+        private void btn_right_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(1, 2);
+            btn_right.Content = FindResource(theBoard.Grid[1, 2].ToString());
+        }
+
+        private void btn_bottomleft_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(2, 0);
+            btn_bottomleft.Content = FindResource(theBoard.Grid[2, 0].ToString());
+        }
+
+        private void btn_bottomcentre_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(2, 1);
+            btn_bottomcentre.Content = FindResource(theBoard.Grid[2, 1].ToString());
+        }
+
+        private void btn_bottomright_Click(object sender, RoutedEventArgs e)
+        {
+            theBoard.PlaceMarker(2, 2);
+            btn_bottomright.Content = FindResource(theBoard.Grid[2, 2].ToString());
         }
     }
 }
