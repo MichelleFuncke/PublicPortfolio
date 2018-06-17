9b213ec257f7db52606cc0d8f3b5fbf84dce105c
N76VZ\MichelleF <kogelmichelle@gmail.com>
2018-06-17 17:40:05 -0400

    Added two crossword examples. These can be used for future changes. These will be moved to xml/json files later. Removed the practice textbox from the GUI.

--- CrossWords/Crossword/Crossword/MainWindow.xaml.cs
+++ CrossWords/Crossword/Crossword/MainWindow.xaml.cs
@@ -57,14 +57,71 @@ namespace Crossword
                 return;
             }
 
-            CreateGrid(10, 10, 50);
-            bool[,] ControlPresent = new bool[10, 10];
+            int colCount, rowCount;
+            var theList = Puzzle2(out colCount, out rowCount);
+
+            PopulateGrid(colCount, rowCount, theList);
+        }
+
+        private List<PuzzleWord> Puzzle1(out int colCount, out int rowCount)
+        {
+            colCount = 18;
+            rowCount = 9;
+            CreateGrid(colCount, rowCount, 50);
+
             spMainPage.Children.Add(CrossWordGrid);
 
             //Read words from a file
             List<PuzzleWord> theList = new List<PuzzleWord>();
-            theList.Add(new PuzzleWord("CAT", 1, "blabla", Direction.across.ToString(), 0, 2));
-            theList.Add(new PuzzleWord("CAN", 1, "blabla", Direction.down.ToString(), 1, 1));
+            //across
+            theList.Add(new PuzzleWord("MOTORWORKS", 1, "Get a $13.95 oil charge", Direction.across.ToString(), 8, 2));
+            theList.Add(new PuzzleWord("CARPETLANDUSA", 2, "With your tax refund get carpet", Direction.across.ToString(), 0, 4));
+            theList.Add(new PuzzleWord("CULVERS", 3, "Remodeling to serve you better", Direction.across.ToString(), 0, 6));
+            theList.Add(new PuzzleWord("HEWITT", 4, "Jackson ____. Pay $50 less this year", Direction.across.ToString(), 12, 6));
+            theList.Add(new PuzzleWord("VINNYSPIZZA", 5, "Home of the 24\" hurricane pizza", Direction.across.ToString(), 4, 8));
+
+            //down
+            theList.Add(new PuzzleWord("BELLAS", 1, "Featuring a \"Fairy Dust Fun Station\"", Direction.down.ToString(), 6, 1));
+            theList.Add(new PuzzleWord("GIORDANOS", 2, "\"Love Takes Many Forms\"", Direction.down.ToString(), 9, 0));
+            theList.Add(new PuzzleWord("ROSS", 3, "Hanser ___. A leader in eye care", Direction.down.ToString(), 11, 1));
+            theList.Add(new PuzzleWord("MIKIMOTO", 4, "Japanese steakhouse in Dekalls featuring hibachi dinners", Direction.down.ToString(), 16, 0));
+
+            return theList;
+        }
+
+        private List<PuzzleWord> Puzzle2(out int colCount, out int rowCount)
+        {
+            colCount = 18;
+            rowCount = 13;
+            CreateGrid(colCount, rowCount, 50);
+
+            spMainPage.Children.Add(CrossWordGrid);
+
+            //Read words from a file
+            List<PuzzleWord> theList = new List<PuzzleWord>();
+            //across
+            theList.Add(new PuzzleWord("ULLRICH", 1, "__ Law, Estate Planning, Wills, Trusts, Elder law", Direction.across.ToString(), 10, 2));
+            theList.Add(new PuzzleWord("FUNME", 2, "Excursions and Entertainment", Direction.across.ToString(), 1, 3));
+            theList.Add(new PuzzleWord("DENTAL", 3, "__ Fields. Family & Cosmetic Dentistry", Direction.across.ToString(), 12, 4));
+            theList.Add(new PuzzleWord("GARLISCH", 4, "__ Automotive Services, Inc, $13.50 oil change", Direction.across.ToString(), 3, 5));
+            theList.Add(new PuzzleWord("MEDITERRANEO", 5, "Middle Eastern Sunday Buffet $11.95", Direction.across.ToString(), 3, 8));
+            theList.Add(new PuzzleWord("KISHHEALTHSYSTEM", 6, "Now part of Northwestern Medicine", Direction.across.ToString(), 0, 10));
+            theList.Add(new PuzzleWord("RUBYSASIAN", 7, "__ __ Market & Other Importal Goods", Direction.across.ToString(), 8, 12));
+
+            //down
+            theList.Add(new PuzzleWord("QUALITY", 1, "__ Mattress Warehouse. Get a free queen", Direction.down.ToString(), 1, 6));
+            theList.Add(new PuzzleWord("JAMRAH", 2, "Chicken Shawarma Meal Deal, $7.99", Direction.down.ToString(), 4, 1));
+            theList.Add(new PuzzleWord("LOSSCENTER", 3, "Sycamore Integrated Weight __ __.", Direction.down.ToString(), 8, 3));
+            theList.Add(new PuzzleWord("MULCHWORKS", 4, "Where hundreds receive great service every year", Direction.down.ToString(), 10, 1));
+            theList.Add(new PuzzleWord("HARVEST", 5, "__ Bible Chapel. Good Friday 7pm", Direction.down.ToString(), 13, 0));
+            theList.Add(new PuzzleWord("LEHAN", 6, "We're more than medicine", Direction.down.ToString(), 17, 4));
+
+            return theList;
+        }
+
+        private void PopulateGrid(int colCount, int rowCount, List<PuzzleWord> theList)
+        {
+            bool[,] ControlPresent = new bool[colCount, rowCount];
 
             //foreach word in puzzle
             foreach (PuzzleWord word in theList)
@@ -80,7 +137,7 @@ namespace Crossword
                 foreach (char letter in word.GetLetters())
                 {
                     //Check the textbox doesn't exist
-                    if (ControlPresent[startCol,startRow])
+                    if (ControlPresent[startCol, startRow])
                     {
                         //Check expected letter if the textbox does exist
                         PuzzleLetter theBox = CrossWordGrid.Children.Cast<PuzzleLetter>().Where(i => (Grid.GetRow(i) == startRow) && (Grid.GetColumn(i) == startCol)).FirstOrDefault();
@@ -90,6 +147,11 @@ namespace Crossword
                         if (letter != theBox.ExpectedLetter)
                         {
                             throw new Exception();
+                        }
+
+                        if ((HeaderTemp.GetDefaultNumber(theBox) == "") && ((startCol == word.StartColumn) && (startRow == word.StartRow)))
+                        {
+                            HeaderTemp.SetDefaultNumber(theBox, word.ClueNumber.ToString());
                         }
 
                         //move in the direction
@@ -115,12 +177,10 @@ namespace Crossword
 
                         //move in the direction
                         startCol += directionCol;
-                        startRow += directionRow;   
-                    }                    
+                        startRow += directionRow;
+                    }
                 }
-
-                //Add the clue to a list to display
-            }  
+            }
         }
 
         private void mnuReset_Click(object sender, RoutedEventArgs e)
